e5a673e2e44152285af1b2b5c1c274d635436471
Vitaly <vitaly@3dprinteros.com>
2019-03-19 00:17:38 +0200

    set Custom mod as default

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -480,7 +480,7 @@ class CuraApplication(QtApplication):
         preferences.addPreference("metadata/setting_version", 0)
         preferences.setValue("metadata/setting_version", self.SettingVersion) #Don't make it equal to the default so that the setting version always gets written to the file.
 
-        preferences.addPreference("cura/active_mode", "simple")
+        preferences.addPreference("cura/active_mode", 1)
 
         preferences.addPreference("cura/categories_expanded", "")
         preferences.addPreference("cura/jobname_prefix", True)
